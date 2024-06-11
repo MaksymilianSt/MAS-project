1ca750e4ba19b7f26c56ea97a80363a6ff15e6a1
maksymilianst <maksymilianst02@gmail.com>
2024-06-11 21:20:07 +0200

    create BE

---
+++ MAS-BE/src/main/java/mas/MasBe/MasBeApplication.java
@@ -0,0 +1,13 @@
+package mas.MasBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasBeApplication.class, args);
+    }
+
+}
---
+++ MAS-BE/src/test/java/mas/MasBe/MasBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mas.MasBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
